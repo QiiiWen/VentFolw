7d8b57ee7980ce8a5a6de46b00fb5fba782ecf50
QiiiWen <qwgiok123@gmail.com>
2025-02-09 21:28:12 +0800

    test

---
+++ chatbot.py
@@ -0,0 +1,28 @@
+import os
+#from dotenv import load_dotenv
+from openai import OpenAI
+
+#load_dotenv()
+
+OPEN_AI_API_KEY = os.getenv("OPENAI_API_KEY")
+openai = OpenAI(api_key = OPEN_AI_API_KEY)
+
+messages = [
+    { "role": "system", "content": "You are a sarcastic assistant!" } # testing only, to be changed...
+]
+
+def chat_bot():
+    while True:
+        message = input("User: ")
+        if message.lower() == "quit":
+            break
+        messages.append( { "role": "user", "content": message } )
+        output = openai.chat.completions.create(
+            model="gpt-4o-mini",
+            messages = messages
+        )
+        messages.append({ "role": "assistant", "content": output.choices[0].message.content })
+        print(output.choices[0].message.content)
+
+if __name__ == "__main__":
+    chat_bot()
